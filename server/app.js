661579dc205967f608f8a69c51f99181800b6a3f
Ben Neiswander <ben.neiswander@gmail.com>
2015-05-21 17:25:19 -0400

    baseline

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ module.exports = app; // Export it so it can be require('')'d
 
 // The path of our public directory. ([ROOT]/public)
 var publicPath = path.join(__dirname, '../public');
+var bowerPath = path.join(__dirname, '../bower_components');
 
 // The path of our index.html file. ([ROOT]/index.html)
 var indexHtmlPath = path.join(__dirname, '../index.html');
@@ -20,8 +21,10 @@ var indexHtmlPath = path.join(__dirname, '../index.html');
 // something in our public folder, serve up that file
 // e.g. angular.js, style.css
 app.use(express.static(publicPath));
+app.use(express.static(bowerPath));
 
 // If we're hitting our home page, serve up our index.html file!
 app.get('/', function (req, res) {
     res.sendFile(indexHtmlPath);
 });
+
